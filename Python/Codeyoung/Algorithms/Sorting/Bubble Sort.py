b780abadb2b6b1c1c405e14e6b6d43375e1c126e
BuildTools <unconfigured@null.spigotmc.org>
2022-11-21 19:01:53 -0500

    added new file

--- Python/Codeyoung/Algorithms/Sorting/Bubble Sort.py
+++ Python/Codeyoung/Algorithms/Sorting/Bubble Sort.py
@@ -1,5 +1,5 @@
 # Python program for implementation of Bubble Sort
- 
+
 def bubbleSort(arr):
     n = len(arr)
     # optimize code, so if the array is already sorted, it doesn't need
@@ -11,23 +11,22 @@ def bubbleSort(arr):
         # repeat one time more than needed.
         # Last i elements are already in place
         for j in range(0, n-i-1):
- 
+
             # traverse the array from 0 to n-i-1
             # Swap if the element found is greater
             # than the next element
             if arr[j] > arr[j + 1]:
                 swapped = True
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-         
+
         if not swapped:
             # if we haven't needed to make a single swap, we
             # can just exit the main loop.
             return
- 
- 
+
 # Driver code to test above
 arr = [64, 34, 25, 12, 22, 11, 90]
- 
+
 bubbleSort(arr)
  
 print("Sorted array is:")
---
+++ Python/Codeyoung/Algorithms/Sorting/Weird Question From Codeyoung.py
@@ -0,0 +1,55 @@
+import time
+import random
+
+start = time.time()
+arr = random.sample(range(1, 10000), 1000)
+temp = arr
+print(arr)
+def bubbleSort(arr):
+    n = len(arr)
+    swapped = False
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j + 1]:
+                swapped = True
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        if not swapped:
+            return
+def shellSort(array, a):
+    gap = a // 2
+    while gap > 0:
+        for i in range(gap, a):
+            temp = array[i]
+            j = i
+            while j >= gap and array[j - gap] > temp:
+                array[j] = array[j - gap]
+                j -= gap
+
+            array[j] = temp
+        gap //= 2
+def insertionSort(arr):
+
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
+
+        key = arr[i]
+        j = i-1
+        while j >=0 and key < arr[j] :
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+bubbleSort(arr)
+arr = temp
+print(arr, "\n")
+size = len(arr)
+shellSort(arr, size)
+arr = temp
+print(arr, "\n")
+insertionSort(arr)
+arr = temp
+print(arr, "\n")
+arr.sort()
+print(arr, "\n")
+end = time.time()
+total = end-start
+print("total time took is:", total)
