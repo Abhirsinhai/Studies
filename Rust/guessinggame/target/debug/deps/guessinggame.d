83a964ae02c0d4395a863a13b49a1d12289cbea2
BuildTools <unconfigured@null.spigotmc.org>
2023-04-03 17:06:29 -0400

    new fixes and shit

--- Rust/guessinggame/target/debug/deps/guessinggame.d
+++
@@ -1,5 +0,0 @@
-C:\Users\abhir\Desktop\Code\Rust\guessinggame\target\debug\deps\guessinggame.exe: src\main.rs
-
-C:\Users\abhir\Desktop\Code\Rust\guessinggame\target\debug\deps\guessinggame.d: src\main.rs
-
-src\main.rs:
--- Rust/guessinggame/target/debug/guessinggame.d
+++
@@ -1,1 +0,0 @@
-C:\Users\abhir\Desktop\Code\Rust\guessinggame\target\debug\guessinggame.exe: C:\Users\abhir\Desktop\Code\Rust\guessinggame\src\main.rs
